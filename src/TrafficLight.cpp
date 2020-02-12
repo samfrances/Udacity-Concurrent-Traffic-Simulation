24f7833e362e5a1ec3e198f2bb5b080f3685bbac
Sam Frances <sam@samfrances.uk>
2020-02-12 11:33:56 +0000

    First attempt at task FP.5b

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -40,15 +40,21 @@ TrafficLight::TrafficLight()
     _currentPhase = TrafficLightPhase::red;
 }
 
-/*
-
 void TrafficLight::waitForGreen()
 {
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
     // runs and repeatedly calls the receive function on the message queue.
     // Once it receives TrafficLightPhase::green, the method returns.
+
+    while (true) {
+        TrafficLightPhase phase = _queue.receive();
+        if (phase == TrafficLightPhase::green) {
+            return;
+        }
+    }
 }
 
+/*
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
